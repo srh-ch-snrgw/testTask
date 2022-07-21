7d9c12af2d48ee20329c17489eaaae5871080a25
serhii.kuzmych <serhii.kuzmych@synergy-way.com>
2022-07-21 16:11:02 +0300

    configured with ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
