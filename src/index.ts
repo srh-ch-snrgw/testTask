5dcc7f07db58244d11b7466a0df6661f8c6892f5
serhii.kuzmych <serhii.kuzmych@synergy-way.com>
2022-07-21 17:04:24 +0300

    might be working

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import _ from 'lodash';
 
 function component() {
   const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  const p = document.createElement('p');
+  element.classList.add('root');
+  p.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.append(p);
   return element;
 }
 
@@ -10,8 +13,83 @@ function appendComponent() {
   document.body.appendChild(component());
 }
 
+function testTask() {
+  function makeCounter() {
+    let num = 0;
+
+    function throwError() {
+      throw new Error('handle me');
+    }
+
+    setInterval(() => {
+      try {
+        throwError();
+      } catch (error) {
+        console.log(0);
+      }
+    }, 3000);
+
+    return function () {
+      return num++;
+    };
+  }
+
+  const counter = makeCounter();
+
+  function throttle(func: Function, ms: number, ...args: any[]) {
+    let isThrottled = false;
+    let savedArgs: any;
+    let savedThis: any;
+    let res = 0;
+
+    function wrapper() {
+      if (isThrottled) {
+        savedArgs = arguments;
+        savedThis = this;
+        return res;
+      }
+      isThrottled = true;
+
+      res = func.apply(this, arguments);
+
+      setTimeout(function () {
+        isThrottled = false;
+        if (savedArgs) {
+          res = wrapper.apply(savedThis, savedArgs);
+          savedArgs = savedThis = null;
+        }
+        return res;
+      }, ms);
+
+      return res;
+    }
+
+    return wrapper;
+  }
+
+  const throttledCounter = throttle(counter, 1000);
+
+  const btn = document.createElement('button');
+  btn.innerText = 'increment click';
+
+  btn.onclick = function (e) {
+    const res = throttledCounter();
+    console.log(
+      'num',
+      res,
+      'squared value',
+      Math.pow(res, 2),
+      'square root',
+      Math.sqrt(res)
+    );
+  };
+
+  document.querySelector('.root').appendChild(btn);
+}
+
 function main() {
   appendComponent();
+  testTask();
 }
 
 main();
