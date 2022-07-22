a726af96c32e538b6322b271ba01db1161b52a05
sergiycheck <sergiycheck@gmail.com>
2022-07-22 09:51:04 +0300

    split into files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import throttledCounter from './throttled-counter';
 
 function component() {
   const element = document.createElement('div');
@@ -14,61 +15,6 @@ function appendComponent() {
 }
 
 function testTask() {
-  function makeCounter() {
-    let num = 0;
-
-    function throwError() {
-      throw new Error('handle me');
-    }
-
-    setInterval(() => {
-      try {
-        throwError();
-      } catch (error) {
-        console.log(0);
-      }
-    }, 3000);
-
-    return function () {
-      return num++;
-    };
-  }
-
-  const counter = makeCounter();
-
-  function throttle(func: Function, ms: number, ...args: any[]) {
-    let isThrottled = false;
-    let savedArgs: any;
-    let savedThis: any;
-    let res = 0;
-
-    function wrapper() {
-      if (isThrottled) {
-        savedArgs = arguments;
-        savedThis = this;
-        return res;
-      }
-      isThrottled = true;
-
-      res = func.apply(this, arguments);
-
-      setTimeout(function () {
-        isThrottled = false;
-        if (savedArgs) {
-          res = wrapper.apply(savedThis, savedArgs);
-          savedArgs = savedThis = null;
-        }
-        return res;
-      }, ms);
-
-      return res;
-    }
-
-    return wrapper;
-  }
-
-  const throttledCounter = throttle(counter, 1000);
-
   const btn = document.createElement('button');
   btn.innerText = 'increment click';
 
